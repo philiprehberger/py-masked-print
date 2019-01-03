0349176248af68ff8191b2a0c8d9995dc1bef4d0
Philip Rehberger <me@philiprehberger.com>
2019-01-03 15:15:19 -0500

    Initial release: philiprehberger-masked-print

---
+++ README.md
@@ -0,0 +1,69 @@
+# philiprehberger-masked-print
+
+Automatically mask sensitive values (API keys, passwords, tokens) in logs and print output.
+
+## Install
+
+```bash
+pip install philiprehberger-masked-print
+```
+
+## Usage
+
+```python
+from philiprehberger_masked_print import mask, mask_dict, MaskedFormatter
+
+# Mask a single string
+masked = mask("sk-abc123secret456xyz")
+# "sk-a*************xyz"
+```
+
+### Mask dictionaries
+
+```python
+config = {
+    "host": "localhost",
+    "password": "super-secret-value",
+    "database": {
+        "connection_string": "postgres://admin:pass@localhost/db",
+    },
+}
+
+safe = mask_dict(config)
+# {
+#     "host": "localhost",
+#     "password": "supe***********lue",
+#     "database": {
+#         "connection_string": "post*****************/db",
+#     },
+# }
+```
+
+### Auto-mask log output
+
+```python
+import logging
+from philiprehberger_masked_print import MaskedFormatter
+
+handler = logging.StreamHandler()
+handler.setFormatter(MaskedFormatter("%(levelname)s: %(message)s"))
+
+logger = logging.getLogger("app")
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+logger.info("Using key sk-proj-abc123def456ghi789jkl012mno")
+# INFO: Using key sk-p*************************mno
+```
+
+## API
+
+| Function / Class | Description |
+|---|---|
+| `mask(value, *, show_first=4, show_last=3, mask_char="*")` | Mask a string, keeping the first and last N characters visible |
+| `mask_dict(data, *, sensitive_keys=None, show_first=4, show_last=3)` | Recursively mask sensitive key values in a dictionary |
+| `MaskedFormatter(fmt)` | Logging formatter that auto-redacts secret patterns (sk-..., eyJ..., AKIA..., URL credentials) |
+
+## License
+
+MIT
---
+++ src/philiprehberger_masked_print/__init__.py
@@ -0,0 +1,150 @@
+"""Automatically mask sensitive values (API keys, passwords, tokens) in logs and print output."""
+
+from __future__ import annotations
+
+import logging
+import re
+
+__all__ = ["mask", "mask_dict", "MaskedFormatter"]
+
+_DEFAULT_SENSITIVE_KEYS: frozenset[str] = frozenset(
+    {
+        "password",
+        "secret",
+        "token",
+        "api_key",
+        "apikey",
+        "authorization",
+        "auth",
+        "credential",
+        "private_key",
+        "access_token",
+        "refresh_token",
+        "database_url",
+        "connection_string",
+        "dsn",
+    }
+)
+
+_SECRET_PATTERNS: list[re.Pattern[str]] = [
+    re.compile(r"sk-[A-Za-z0-9]{20,}"),
+    re.compile(r"eyJ[A-Za-z0-9_-]{10,}"),
+    re.compile(r"AKIA[A-Z0-9]{16}"),
+    re.compile(r"://[^\s:]+:[^\s@]+@"),
+]
+
+
+def mask(
+    value: str,
+    *,
+    show_first: int = 4,
+    show_last: int = 3,
+    mask_char: str = "*",
+) -> str:
+    """Mask a string, preserving only the first and last N characters.
+
+    Args:
+        value: The string to mask.
+        show_first: Number of leading characters to keep visible.
+        show_last: Number of trailing characters to keep visible.
+        mask_char: Character used for masking.
+
+    Returns:
+        The masked string. If the value is too short to mask meaningfully,
+        it is fully replaced with mask characters.
+    """
+    if not value:
+        return value
+
+    length = len(value)
+    visible = show_first + show_last
+
+    if length <= visible:
+        return mask_char * length
+
+    masked_length = length - visible
+    return value[:show_first] + mask_char * masked_length + value[length - show_last :]
+
+
+def _is_sensitive_key(key: str, sensitive_keys: frozenset[str]) -> bool:
+    """Check if a key matches any sensitive key via case-insensitive substring match."""
+    lower = key.lower()
+    return any(s in lower for s in sensitive_keys)
+
+
+def mask_dict(
+    data: dict[str, object],
+    *,
+    sensitive_keys: frozenset[str] | set[str] | list[str] | None = None,
+    show_first: int = 4,
+    show_last: int = 3,
+) -> dict[str, object]:
+    """Recursively mask values whose keys match sensitive key patterns.
+
+    Args:
+        data: The dictionary to process.
+        sensitive_keys: Key substrings to treat as sensitive. Defaults to a
+            built-in set covering common secret field names.
+        show_first: Number of leading characters to keep visible in masked values.
+        show_last: Number of trailing characters to keep visible in masked values.
+
+    Returns:
+        A new dictionary with sensitive string values masked.
+    """
+    keys = frozenset(sensitive_keys) if sensitive_keys is not None else _DEFAULT_SENSITIVE_KEYS
+    return _mask_dict_recursive(data, keys, show_first, show_last)
+
+
+def _mask_dict_recursive(
+    data: dict[str, object],
+    sensitive_keys: frozenset[str],
+    show_first: int,
+    show_last: int,
+) -> dict[str, object]:
+    result: dict[str, object] = {}
+    for key, value in data.items():
+        if isinstance(value, dict):
+            result[key] = _mask_dict_recursive(value, sensitive_keys, show_first, show_last)
+        elif isinstance(value, str) and _is_sensitive_key(key, sensitive_keys):
+            result[key] = mask(value, show_first=show_first, show_last=show_last)
+        else:
+            result[key] = value
+    return result
+
+
+class MaskedFormatter(logging.Formatter):
+    """A logging formatter that automatically masks secret patterns in log messages.
+
+    Detected patterns:
+        - ``sk-...`` (OpenAI-style API keys)
+        - ``eyJ...`` (JWT tokens)
+        - ``AKIA...`` (AWS access key IDs)
+        - ``://user:pass@`` (credentials in URLs)
+
+    Usage::
+
+        import logging
+        from philiprehberger_masked_print import MaskedFormatter
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(MaskedFormatter("%(levelname)s: %(message)s"))
+        logger = logging.getLogger(__name__)
+        logger.addHandler(handler)
+    """
+
+    def format(self, record: logging.LogRecord) -> str:  # noqa: A003
+        message = super().format(record)
+        return self._mask_secrets(message)
+
+    @staticmethod
+    def _mask_secrets(text: str) -> str:
+        for pattern in _SECRET_PATTERNS:
+            text = pattern.sub(_redact_match, text)
+        return text
+
+
+def _redact_match(m: re.Match[str]) -> str:
+    matched = m.group(0)
+    if matched.startswith("://"):
+        return "://***:***@"
+    return mask(matched)
